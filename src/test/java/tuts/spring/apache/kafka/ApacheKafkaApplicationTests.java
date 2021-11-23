70d8903d97ddb24f52c291d7ca8f513a5250ad94
Kingsley Victor <javaprodigy56@gmail.com>
2021-11-23 13:34:11 +0100

    init repository

---
+++ src/test/java/tuts/spring/apache/kafka/ApacheKafkaApplicationTests.java
@@ -0,0 +1,11 @@
+package tuts.spring.apache.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApacheKafkaApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
