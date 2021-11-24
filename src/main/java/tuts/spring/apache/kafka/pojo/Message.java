65fa3f4f861ec29f39517cb1c95e9cfa79d2c683
Kingsley Victor <javaprodigy56@gmail.com>
2021-11-24 11:19:17 +0100

    add configuration

--- src/main/java/tuts/spring/apache/kafka/pojo/Message.java
+++ src/main/java/tuts/spring/apache/kafka/pojo/Message.java
@@ -1,9 +1,12 @@
 package tuts.spring.apache.kafka.pojo;
 
-public class Message {
+public class Message implements java.io.Serializable {
 
   private String name;
   private String content;
+
+  // For serialization
+  public Message() {}
 
   public Message(final String name, final String content) {
     this.name = name;
