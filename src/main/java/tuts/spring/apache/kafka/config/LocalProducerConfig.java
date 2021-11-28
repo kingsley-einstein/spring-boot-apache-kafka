9d8a64e55e95748b107e3a49ae539b41c25ef1d9
Kingsley Victor <javaprodigy56@gmail.com>
2021-11-28 23:42:54 +0100

    update config

--- src/main/java/tuts/spring/apache/kafka/config/LocalProducerConfig.java
+++ src/main/java/tuts/spring/apache/kafka/config/LocalProducerConfig.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.support.serializer.JsonSerializer;
 import tuts.spring.apache.kafka.pojo.Message;
@@ -25,5 +26,10 @@ public class LocalProducerConfig {
     config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
     config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
     return new DefaultKafkaProducerFactory<>(config);
+  }
+
+  @Bean
+  public KafkaTemplate<String, Message> kafkaTemplate() {
+    return new KafkaTemplate<>(producerFactory());
   }
 }
